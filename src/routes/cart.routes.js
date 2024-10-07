f18962db3c6eb19f829949b86c60068418b6b505
yanil8068 <yanil8068@gmail.com>
2024-10-07 16:21:07 +0530

    removed comments

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,27 +1,3 @@
-// import express from "express";
-// import { authentication } from "../middleware/user.middleware.js";
-// import {
-//   addToCart,
-//   getCart,
-//   updateCartItem,
-//   removeCartItem,
-//   emptyCart,
-// } from "../controllers/cart.controller.js";
-
-// const cartRouter = express.Router();
-
-// // Middleware to ensure the user is authenticated
-// cartRouter.use(authentication);
-
-// // Routes for cart operations
-// cartRouter.post("/add", addToCart);
-// cartRouter.get("/", getCart);
-// cartRouter.put("/update", updateCartItem);
-// cartRouter.delete("/empty", emptyCart);
-// cartRouter.delete("/remove", removeCartItem);
-
-// export default cartRouter;
-
 import express from "express";
 import { authentication } from "../middleware/user.middleware.js";
 import {
